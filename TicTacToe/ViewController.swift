37c0db8026638f9966f54072740237ae9718213b
Taewon Kong <tkong9@gatech.edu>
2019-09-05 14:40:13 -0400

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by TAEWON KONG on 9/5/19.
+//  Copyright © 2019 TAEWON KONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
